f8b6336f4af4171b21dcf03fed92385259ab6407
Renan Ferreira da Silva <renancontazps98@gmail.com>
2025-12-10 15:13:14 -0300

    mensagens

--- src/app/controllers/SystemController.js
+++ src/app/controllers/SystemController.js
@@ -74,6 +74,84 @@ class SystemController {
 
     return res.status(201).json(idEspecialidade);
   }
+
+
+  
+
+   async listarmensagens(req, res) {
+    const pool = await UtilsFF.conectarSQLServer();
+    if (!pool) return res.status(500).json({ error: "Falha na conexão com o banco de dados." });
+
+    let baseQuery = `
+            SELECT id_colaborador, id_especialidade, mensagem_paciente, data, nome_paciente, email_paciente, telefone_paciente FROM mensagens;
+        `;
+
+    const request = pool.request();
+    const result = await request.query(baseQuery);
+
+    if (!result.recordset.length) {
+      return res.status(404).json({ message: "Nenhum registro encontrado." });
+    }
+
+    return res.status(200).json(result.recordset);
+  }
+
+  async obterIdmensagens(req, res) {
+    const pool = await UtilsFF.conectarSQLServer();
+    if (!pool) return res.status(500).json({ error: "Falha na conexão com o banco de dados." });
+
+    const idmensagens = req.params.id;
+
+    let baseQuery = `
+            SELECT id_colaborador, id_especialidade, mensagem_paciente, data, nome_paciente, email_paciente, telefone_paciente
+              FROM mensagens
+             WHERE id = @idMensagens ;
+        `;
+
+    const request = pool.request();
+    const result = await request
+      .input("idmensagens", idmensagens)
+      .query(baseQuery);
+
+    if (!result.recordset.length) {
+      return res.status(404).json({ message: "Nenhum registro encontrado." });
+    }
+
+    return res.status(200).json(result.recordset);
+  }
+
+  async inserirmensagens(req, res) {
+    const pool = await UtilsFF.conectarSQLServer();
+    if (!pool) return res.status(500).json({ error: "Falha na conexão com o banco de dados." });
+
+    const mensagens = req.body.descricao;
+
+    let baseQuery = `
+            INSERT INTO mensagens(id_colaborador, id_especialidade, mensagem_paciente, data, nome_paciente, email_paciente, telefone_paciente)
+            OUTPUT INSERTED.id
+            VALUES(@mensagens);
+        `;
+
+    const request = pool.request();
+    const result = await request
+      .input("mensagens", mensagens.mensagem_paciente)
+      .query(baseQuery);
+
+    if (!result.recordset.length) {
+      return res.status(404).json({ message: "Nenhum registro encontrado." });
+    }
+
+    const idmensagens = result.recordset?.[0]?.id || null;
+
+    if (!idmensagens) {
+      return res.status(400).json({ message: "Falha ao obter o ID" });
+    }
+
+    return res.status(201).json(idmensagens);
+  }
+
 }
 
 module.exports = new SystemController();
+
+
